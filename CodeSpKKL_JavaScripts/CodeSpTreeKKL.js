cc896098715fa444bb72eb1bdb3e464a2ee98339
yoskoyoav <yoskoyoav@gmail.com>
2025-05-20 13:22:59 +0300

    Add species list JavaScript files for SMY form

---
+++ CodeSpKKL_JavaScripts/CodeSpTreeKKL.js
@@ -0,0 +1,340 @@
+/*
+ * JavaScript functions for Survey123
+ * Last update: 17/02/2025 by Yoav Yoskovich
+*/
+
+function CodeSpTreeKKL(code)
+{
+    
+    var Sp;
+    // Add your code here
+    switch(code)
+    {
+           case "2450": 
+                  Sp="אגוזים";
+                  break;
+           case "3011": 
+                  Sp="אגס סורי";
+                  break;
+           case "3021": 
+                  Sp="אדר סורי";
+                  break;
+	    case "4101": 
+                  Sp="אוג הבורסקאים";
+                  break;
+           case "50036": 
+                  Sp="אוג קוצני";
+                  break;
+           case "2306": 
+                  Sp="אולמוס שעיר";
+                  break;
+           case "1103": 
+                  Sp="אורן ברוטיה";
+                  break;
+           case "1104": 
+                  Sp="אורן הצנובר (גלעין)";
+                  break;
+           case "1105": 
+                  Sp="אורן ירושלים";
+                  break;
+           case "1106": 
+                  Sp="אורן כנארי";
+                  break;
+           case "1101": 
+                  Sp="אורן רדיאטה (מקרין)";
+                  break;
+           case "1100": 
+                  Sp="אורנים";
+                break;
+	    case "2309": 
+                  Sp="אזדרכת מצויה";
+		  break;
+	    case "2308": 
+                  Sp="אילנתה בלוטית";
+		  break;
+           case "3031": 
+                  Sp="אלה אטלנטית";
+                  break;
+           case "3032": 
+                  Sp="אלה אמיתית";
+                  break;
+           case "3033": 
+                  Sp="אלה ארץ - ישראלית";
+                  break;
+           case "3042": 
+                  Sp="אלון מצוי";
+                  break;
+           case "3044": 
+                  Sp="אלון תבור";
+                  break;
+           case "3045": 
+                  Sp="אלון תולע";
+                  break;
+           case "2103": 
+                  Sp="אקליפטוס אוקסידנטליס";
+                  break;
+           case "2108": 
+                  Sp="אקליפטוס גומפוצפלה";
+                  break;
+           case "50038": 
+                  Sp="אקליפטוס דונדס";
+                  break;
+           case "2113": 
+                  Sp="אקליפטוס המקור";
+                  break;
+           case "2117": 
+                  Sp="אקליפטוס טורקוואטה";
+                  break;
+	    case "50024": 
+                  Sp="אקליפטוס יוטיליס (שימושי)";
+                  break;
+	    case "2116": 
+                  Sp="אקליפטוס כתום ('טורווד')";
+                  break;
+           case "2118": 
+                  Sp="אקליפטוס לאוקוקסילון";
+                  break;
+	    case "50032": 
+                  Sp="אקליפטוס לסאופי";
+                  break;
+           case "2126": 
+                  Sp="אקליפטוס סטריקלנדי";
+                  break;
+           case "2129": 
+                  Sp="אקליפטוס ספטולטה";
+                  break; 
+	    case "50030": 
+                  Sp="אקליפטוס סלובריס (אקליפטוס חיוני)";
+                  break;
+           case "2130": 
+                  Sp="אקליפטוס סרג'נטי";
+                  break;
+	    case "50023": 
+                  Sp="אקליפטוס פופולנאה (צפצפתי)";
+                  break;
+	    case "50022": 
+                  Sp="אקליפטוס פלטיפוס (פחוס עוקץ)";
+                  break;
+	    case "50027": 
+                  Sp="אקליפטוס קמפספה (א. מכסיף)";
+                  break;
+	    case "50031": 
+                  Sp="אקליפטוס קלילנד";
+                  break;
+           case "2100": 
+                  Sp="אקליפטוסים";
+                  break;
+           case "1310": 
+                  Sp="ארזים";
+                  break;
+           case "3051": 
+                  Sp="אשחר רחב-עלים";
+                  break;
+	    case "3061": 
+                  Sp="אשל היאור";
+                  break;
+	    case "3062": 
+                  Sp="אשל הפרקים";
+                  break;
+           case "3060": 
+                  Sp="אשלים";
+                  break;
+           case "3071": 
+                  Sp="בר-זית בינוני";
+                  break;
+           case "1201": 
+                  Sp="ברוש אטלנטי";
+                  break;
+           case "1202": 
+                  Sp="ברוש אריזוני";
+                  break;
+           case "1204": 
+                  Sp="ברוש מצוי";
+                  break;
+	    case "1205": 
+                  Sp="ברוש מקרוקרפה (גדול פרות)";
+                  break;
+           case "1200": 
+                  Sp="ברושים";
+                  break;
+           case "3081": 
+                  Sp="דולב מזרחי";
+                  break;
+           case "6205": 
+                  Sp="ושינגטוניה חוטית";
+                  break;
+           case "6206": 
+                  Sp="ושינגטוניה חסונה";
+                  break;
+           case "3091": 
+                  Sp="זית אירופי";
+                  break;
+           case "2994": 
+                  Sp="זיתים";
+                  break;
+           case "3101": 
+                  Sp="זקום מצרי";
+                  break;
+           case "3111": 
+                  Sp="חרוב מצוי";
+                  break;
+           case "1221": 
+                  Sp="טטרקליניס ארטיקולטה";
+                  break;
+           case "3121": 
+                  Sp="כליל החורש";
+                  break;
+	    case "50033": 
+                  Sp="כילופסיס סרגלי";
+                  break;
+           case "3131": 
+                  Sp="ליבנה רפואי";
+                  break;
+           case "3140": 
+                  Sp="מורינגה";
+                  break;
+           case "3151": 
+                  Sp="מורן החורש";
+                  break;
+           case "1000": 
+                  Sp="מחטניים";
+                  break;
+           case "3161": 
+                  Sp="מילה סורית";
+                  break;
+           case "3171": 
+                  Sp="מיש דרומי";
+                  break;
+           case "50034": 
+                  Sp="נזם קוף מתוק";
+                  break;
+	    case "50028": 
+                  Sp="עוזרר אדום";
+                  break;
+           case "3182": 
+                  Sp="עוזרר קוצני (מצוי)";
+                  break;
+           case "3191": 
+                  Sp="ער אציל (דפנה)";
+                  break;
+           case "3201": 
+                  Sp="ערבה מחודדת";
+                  break;
+           case "6577": 
+                  Sp="פיקוס השדרות";
+                  break;
+           case "3221": 
+                  Sp="פיקוס השיקמה";
+                  break;
+           case "3222": 
+                  Sp="פיקוס התאנה";
+                  break;
+           case "6582": 
+                  Sp="פיקוס קדוש";
+                  break;
+	    case "2381": 
+                  Sp="פלפלון בכות";
+                  break;
+	    case "2382": 
+                  Sp="פלפלון דמוי-אלה";
+                  break;
+           case "2390": 
+                  Sp="פרוזופיס (ינבוט)";
+                  break;
+	    case "7621": 
+                  Sp="צחר כחלחל";
+                  break;
+           case "2411": 
+                  Sp="צפצפה מכסיפה";
+                  break;
+           case "3231": 
+                  Sp="צפצפת הפרת";
+                  break;
+           case "6425": 
+                  Sp="צרסידיום פרחוני";
+                  break;
+           case "2420": 
+                  Sp="קזוארינה";
+                  break;
+           case "3241": 
+                  Sp="קטלב מצוי";
+                  break;
+	    case "1231": 
+                  Sp="קליטריס מיובל";
+                  break;
+           case "2910": 
+                  Sp="רחבי-עלים שונים";
+                  break;
+           case "4196": 
+                  Sp="רימון מצוי";
+                  break;
+           case "3251": 
+                  Sp="שזיף הדב";
+                  break;
+           case "9990": 
+                  Sp="שטח פתוח";
+                  break;
+           case "3261": 
+                  Sp="שיזף מצוי";
+                  break;
+           case "50037": 
+                  Sp="שיזף שעיר";
+                  break;
+           case "2201": 
+                  Sp="שיטה אלטיור";
+                  break;
+	    case "2206": 
+                  Sp="שיטה הורידה";
+                  break;
+	    case "2209": 
+                  Sp="שיטה ויקטוריה";
+                  break;
+	    case "2211": 
+                  Sp="שיטה כחלחלה";
+                  break;
+	    case "2222": 
+                  Sp="שיטה מחטנית (סקלרוספרמה)";
+                  break;
+           case "2215": 
+                  Sp="שיטה מלבינה";
+                  break;
+           case "2218": 
+                  Sp="שיטה סוככנית";
+                  break;
+           case "2221": 
+                  Sp="שיטה סלילנית";
+                  break;
+	    case "2217": 
+                  Sp="שיטה סליצינה";
+                  break;
+           case "2229": 
+                  Sp="שיטה עגולת-זרעים";
+                  break;
+	    case "2244": 
+                  Sp="שיטה צהובת גזע (קסנטופלאה)";
+                  break;
+           case "2200": 
+                  Sp="שיטים";
+                  break;
+           case "2231": 
+                  Sp="שיטת הנגב";
+                  break;
+           case "3271": 
+                  Sp="שקד מצוי";
+                  break;
+           case "3272": 
+                  Sp="שקד קטן-עלים";
+                  break;
+           case "2440": 
+                  Sp="תות";
+                  break;
+           case "6241": 
+                  Sp="תמר מצוי";
+		    break;
+            default:
+                  Sp=""
+    }        
+    
+    return Sp;
+}
+
