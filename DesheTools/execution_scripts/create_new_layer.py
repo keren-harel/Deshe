e4763b0f9a9712ab222aab376b4f7c742ac22fc1
keren-harel <kerengold.kg@gmail.com>
2025-05-21 11:22:18 +0300

    Fix reload

--- DesheTools/execution_scripts/create_new_layer.py
+++ DesheTools/execution_scripts/create_new_layer.py
@@ -5,23 +5,27 @@ from pathlib import Path
 from importlib import reload
 
 
-ROOT = str(Path(__file__).parents[1].absolute())
-sys.path.append(str(ROOT))
+DESHE_TOOLS_FOLDER_PATH = str(Path(__file__).parents[1].absolute())
 
-import utils.table_utils as pyt_reload_table_utils
+def add_to_root(folders):
+    if DESHE_TOOLS_FOLDER_PATH not in sys.path:
+        sys.path.insert(0, DESHE_TOOLS_FOLDER_PATH)
+    for folder in folders:
+        arcpy.AddMessage(f"start! {folder}")
+        if rf"{DESHE_TOOLS_FOLDER_PATH}\{folder}" not in sys.path:
+            arcpy.AddMessage(f"here! {folder}")
+            sys.path.insert(1, rf"{DESHE_TOOLS_FOLDER_PATH}\{folder}")
 
-[
-    print(f"Reloaded {reload(module).__name__}")
-    for module_name, module in globals().items()
-    if module_name.startswith("pyt_reload")
-]
 
+add_to_root(['utils', 'enums'])
 
-import utils.table_utils as table_utils
-import enums.excel_values as excel_values
-
+import table_utils
+reload(table_utils)
+import excel_values
+reload(excel_values)
 
 SHEET_NAME = "table_modification"
+
 
 def execute(layer_name):
 
@@ -29,7 +33,7 @@ def execute(layer_name):
 
     aprx = arcpy.mp.ArcGISProject("CURRENT")
     gdb_path = aprx.defaultGeodatabase
-    configuration_path = os.path.join(ROOT, "configuration")
+    configuration_path = os.path.join(DESHE_TOOLS_FOLDER_PATH, "configuration")
     excel_path = os.path.join(configuration_path, "fields.xlsx")
 
     arcpy.env.overwriteOutput = True
@@ -38,9 +42,9 @@ def execute(layer_name):
                                         "Polyline",
                                         spatial_reference=SPATIAL_REFERENCE)
 
-    df = pyt_reload_table_utils.load_excel_data(excel_path, SHEET_NAME)
+    df = table_utils.load_excel_data(excel_path, SHEET_NAME)
     layer_df = df[df[excel_values.ExcelColumns.TABLE_NAME.value] == layer_name]
-    pyt_reload_table_utils.add_fields_to_layer_from_excel(layer_df, layer_name, gdb_path)
+    table_utils.add_fields_to_layer_from_excel(layer_df, layer_name, gdb_path)
 
     aprx = arcpy.mp.ArcGISProject("CURRENT")
     active_map = aprx.activeMap
--- DesheTools/execution_scripts/modify_layer.py
+++ DesheTools/execution_scripts/modify_layer.py
@@ -4,52 +4,45 @@ import os
 from pathlib import Path
 from importlib import reload
 
-ROOT = str(Path(__file__).parents[1].absolute())
-SHEET_NAME = "table_modification"
+DESHE_TOOLS_FOLDER_PATH = str(Path(__file__).parents[1].absolute())
 
 def add_to_root(folders):
-    if ROOT not in sys.path:
-        sys.path.insert(0, ROOT)
+    if DESHE_TOOLS_FOLDER_PATH not in sys.path:
+        sys.path.insert(0, DESHE_TOOLS_FOLDER_PATH)
     for folder in folders:
-        if rf"{ROOT}\{folder}" not in sys.path:
-            sys.path.insert(1, rf"{ROOT}\{folder}")
+        arcpy.AddMessage(f"start! {folder}")
+        if rf"{DESHE_TOOLS_FOLDER_PATH}\{folder}" not in sys.path:
+            arcpy.AddMessage(f"here! {folder}")
+            sys.path.insert(1, rf"{DESHE_TOOLS_FOLDER_PATH}\{folder}")
 
-add_to_root(['utils', 'enums', 'configuration'])
 
-# Import dynamic modules with pyt_reload prefix
-import utils.table_utils as pyt_reload_table_utils
-import enums.excel_values as pyt_reload_excel_values
+add_to_root(['utils', 'enums'])
 
-# Inline reloader of dynamic modules
-[
-    print(f"Reloaded {reload(module).__name__}")
-    for module_name, module in globals().items()
-    if module_name.startswith("pyt_reload")
-]
+import table_utils
+reload(table_utils)
+import excel_values
+reload(excel_values)
 
-# Import the Tool Importer function
-from utils.table_utils import *
-from enums.excel_values import LayerNameExcel
-
+SHEET_NAME = "table_modification"
 
 def execute(layer_name_excel,layer_path):
 
-    configuration_path = os.path.join(ROOT, "configuration")
+    configuration_path = os.path.join(DESHE_TOOLS_FOLDER_PATH, "configuration")
     excel_path = os.path.join(configuration_path, "fields.xlsx")
     gdb_path = os.path.dirname(layer_path)
     layer_name = os.path.basename(layer_path)
 
-    df = load_excel_data(excel_path, SHEET_NAME)
-    layer_df = df[df[ExcelColumns.TABLE_NAME.value] == layer_name_excel]
+    df = table_utils.load_excel_data(excel_path, SHEET_NAME)
+    layer_df = df[df[excel_values.ExcelColumns.TABLE_NAME.value] == layer_name_excel]
 
-    is_verified = verify_required_fields(layer_path, layer_df)
+    is_verified = table_utils.verify_required_fields(layer_path, layer_df)
     if not is_verified:
         return
 
-    remove_extra_fields_from_layer(layer_df, layer_path)
+    table_utils.remove_extra_fields_from_layer(layer_df, layer_path)
 
-    to_add_df = layer_df[layer_df[ExcelColumns.TO_ADD.value].notna()]
-    add_fields_to_layer_from_excel(to_add_df, layer_name, gdb_path)
+    to_add_df = layer_df[layer_df[excel_values.ExcelColumns.TO_ADD.value].notna()]
+    table_utils.add_fields_to_layer_from_excel(to_add_df, layer_name, gdb_path)
 
 
 if __name__ == "__main__":
